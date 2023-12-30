e2d151a614e2a0c4df6cfed93285e7c7d46ebf5b
KentaWatanabe <nabesuke5098@gmail.com>
2023-12-30 15:45:58 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+// mod vars;
+mod stack_heap;
+
+fn main() {
+        // println!("Hello, world!");
+        // vars::run();
+        stack_heap::run();
+        // vars::sub_a::func_a();
+        // vars::sub_b::func_b();
+}
---
+++ src/vars/sub_b.rs
@@ -0,0 +1,4 @@
+pub fn func_b() {
+  println!("func b called")
+
+}
