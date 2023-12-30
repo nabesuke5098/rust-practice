3ff868061379b2014cc67a77aeb3c06440108b4e
KentaWatanabe <nabesuke5098@gmail.com>
2023-12-31 07:02:16 +0900

    practice_12_30

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 // mod vars;
-mod stack_heap;
+// mod stack_heap;
+// mod ownership;
+mod generics;
 
 fn main() {
         // println!("Hello, world!");
         // vars::run();
-        stack_heap::run();
+        // stack_heap::run();
+        // ownership::run();
+        generics::run();
         // vars::sub_a::func_a();
         // vars::sub_b::func_b();
 }
