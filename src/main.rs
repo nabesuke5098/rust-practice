11dbb78a643835d678e7401c1f7487c48525fcc8
KentaWatanabe <nabesuke5098@gmail.com>
2023-12-31 13:31:11 +0900

    add library

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 // mod vars;
 // mod stack_heap;
 // mod ownership;
-mod generics;
+// mod generics;
+// mod lifetime;
+// mod structs;
+// mod enums;
+// mod traits;
+// mod error_handling;
+mod unit_test;
+mod debug;
+extern crate lib_demo;
 
 fn main() {
         // println!("Hello, world!");
         // vars::run();
         // stack_heap::run();
         // ownership::run();
-        generics::run();
+        // generics::run();
+        // lifetime::run();
+        // structs::run();
+        // enums::run();
+        // traits::run();
+        // error_handling::run();
+
         // vars::sub_a::func_a();
         // vars::sub_b::func_b();
+        lib_demo::print_random_number();
+        debug::run();
 }
